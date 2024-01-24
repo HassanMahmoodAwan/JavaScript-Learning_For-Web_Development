5adbde1aab266572648be8d3d47e3261b8bf4990
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-01-24 20:28:33 +0000

    DataTypes Advance

---
+++ 03_Advance_DataTypes/confusing_dt.js
@@ -0,0 +1,27 @@
+console.log("3" + 3)
+console.log("3" - 3)
+
+console.log("3" + 3 + 2)
+console.log(3 + 3 + "2")
+
+
+//  Numeric() === +
+console.log(+true)
+console.log(+"")
+console.log(+" ")
+
+
+// Variables
+let num1, num2, num3
+num1 = num2 = num3 = 10 + "3"
+
+
+// MDN -> Pre-Increment && Post-Increment;
+
+let n01 = 3
+n01++
+console.log(n01)
+
+let no2 = 2
+++no2
+console.log(no2)
