7c8f2af0e8a1639bcd1238c0c2ea0c713f26e4a3
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-01-26 19:57:47 +0000

    Functions statrted

---
+++ 10_Functions/01_basics.js
@@ -0,0 +1,31 @@
+function logMessage(username){
+    return `${username} has successfully logged!`
+}
+console.log(logMessage())             // Undefined 
+const result = logMessage("Hassan")
+console.log(`Result: ${result}`)
+
+
+function loginMessage(userName = 'No Username'){
+    if (userName === undefined || !userName){
+        console.log("No UserName Registered")
+        return
+    }
+    return `${userName} has successfully logged in!`
+}
+const ourResult = loginMessage(userName = "Hassan")
+console.log(`New Result: ${ourResult}`)
+
+
+// Rest Operator same as Spread Operator.
+const userCart = function (...prices){     //having unknown Nos args. 
+    return prices
+}
+userCart(10, 20, 30, 32)
+
+
+
+
+
+
+
