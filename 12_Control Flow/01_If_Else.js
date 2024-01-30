3077e3da43a348fa06456834e47b3e152b6ca4d5
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-01-30 19:20:57 +0000

    If-Else Statement

---
+++ 12_Control Flow/01_If_Else.js
@@ -0,0 +1,23 @@
+// Comparison Operator ->  ==, ===
+// Relational Operator ->  <, >, >=, <=, !=,!==
+// Logical Operator -> &&, ||, !
+
+
+// Note: Comparisons and Relational Operators behave differently.
+
+const balance = 1000
+const user = true
+
+if (balance >= 500 || user){
+    const reward = true
+    console.log(reward)
+}
+console.log(reward)         // Error as no Scope.
+
+
+// if statement short-hand.
+if (user) console.log("User is logged in.")
+
+if (balance >= 500) console.log("Balance is greater than 500."),
+console.log("user is not logged in.")   // Unreadable Code.
+
