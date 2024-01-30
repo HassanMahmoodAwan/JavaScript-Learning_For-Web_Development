e3b5d989970a7230950bdf7788b7fe609110858f
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-01-30 20:14:27 +0000

    Truthy Values

---
+++ 12_Control Flow/03_Truthy.js
@@ -0,0 +1,43 @@
+const userWebsite = "HassanAwan.ai"
+
+if (userWebsite){
+    console.log(userWebsite)
+}                               // It works
+
+// Falsy Values:
+// => 0, -0, NaN, undefined, null, "", false, bigInt 0n
+
+// Truthy Values:
+// => "0", "false", 1, -1, " ", [], {}, function(){}, "abc..."
+
+
+// how to find Empty Array:
+const myArr = new Array()
+
+if(myArr.length === 0){
+    console.log("Array is Empty.")
+}
+
+// How to find Object is empty:
+const myObj = {}
+
+if(Object.keys(myObj).length === 0){
+    console.log("Object is Empty.")
+}
+
+
+// ===== Nullish Coalescing Operators (??) -> Null, Undefined =====
+let var1;
+
+var1 = 4 ?? 19
+console.log(var1)                   // 4
+
+var1 = null ?? 19
+console.log(var1)                   // 19
+
+var1 = undefined ?? 19              // 19
+console.log(var1)
+
+
+// Ternary Operators:
+var1 >= 4 ? console.log(true) : console.log(false)
