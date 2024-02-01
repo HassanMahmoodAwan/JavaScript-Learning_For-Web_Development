c58bec6fb8e06e7db1f4b1278262473e1729d5cf
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-01 12:30:09 +0000

    ForEach Loop

--- 13_Iterations/04_forEach.js
+++ 13_Iterations/04_forEach.js
@@ -1,6 +1,34 @@
-// for Each Loop
+// for Each Loop: Works on Array, String.
+// ==> Callbacks function.
 
 const newArray = [1, 2, 3, 4, 5, 6]
 newArray.forEach(function (item){
     console.log(item)
 } )
+
+const myArray = new Array(10, 12, 15)
+myArray.forEach(function (item, index, array){
+    console.log(item + " at index: " +  index)
+    console.log(array)
+})
+
+myArray.forEach((items)=>(console.log(items)))  // Works.
+
+const languages = [
+    {
+        language: "Python",
+        extension: ".py"
+    },
+    {
+        language: "Javascript",
+        extension: ".JS"
+    },
+    {
+        language: "RUST",
+        extension: ".RS"
+    },   
+]
+
+languages.forEach((items, index)=>{
+    console.log(items["language"])
+})
