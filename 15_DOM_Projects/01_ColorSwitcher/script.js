5509b2fdb6a5a6450386e82d42b99c3f4a206763
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-02 15:15:53 +0500

    Project 01 DOM Completed Color Switches

--- 15_DOM_Projects/01_ColorSwitcher/script.js
+++ 15_DOM_Projects/01_ColorSwitcher/script.js
@@ -1,13 +1,10 @@
-const boxes = document.querySelector("#colorBoxes")
-const allBoxes = Array.from(boxes.children)
-console.log(boxes)
-console.log(allBoxes)
+const body = document.querySelector("body")
+console.log(body)
+const allBoxes = document.querySelectorAll(".box")    // Return NodeList
 
 
-// allBoxes.forEach( ()=>{
-//     const 
-// })
-
-const graber = ()=>{
-    
-}
+allBoxes.forEach( (item)=>{
+    item.addEventListener('click', (event)=>{
+        body.style.backgroundColor = event.target.style.backgroundColor
+    })
+})
