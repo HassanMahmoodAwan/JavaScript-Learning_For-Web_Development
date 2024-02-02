756294a88dced523088665c74f9333ba6e7c37c1
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-02 14:36:00 +0500

    Project 01 Dom Working Progress

---
+++ 15_DOM_Projects/01_ColorSwitcher/script.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelector("#colorBoxes")
+const allBoxes = Array.from(boxes.children)
+console.log(boxes)
+console.log(allBoxes)
+
+
+// allBoxes.forEach( ()=>{
+//     const 
+// })
+
+const graber = ()=>{
+    
+}
