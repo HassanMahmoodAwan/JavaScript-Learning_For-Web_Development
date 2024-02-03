1e3d0415125736000da84663b596f6c233ee57e2
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-04 01:43:45 +0500

    Update 01_Promises.js

--- 18_AysncConepts/01_Promises.js
+++ 18_AysncConepts/01_Promises.js
@@ -2,7 +2,9 @@
 ==> To Avoid CallBack Hell.
 ==> Take callback (Resolve) and (Reject).
 ==> .catch(), .then(), .finally()
+==> Introduce By blueBird
 
+Note: Run this File everyTime.
 */ 
 
 const promiseOne = new Promise( (resolve, reject)=>{
@@ -14,10 +16,40 @@ const promiseOne = new Promise( (resolve, reject)=>{
 
 const promiseTwo = new Promise( (resolve, reject)=>{
     setTimeout( ()=>{
-        console.log("My Promise")                       // Printed
+        console.log("My Promise Two")                       // Printed
         resolve()                       
     }, 1000)
 })
-
 promiseTwo
+.then( ()=>{
+    console.log("Resolved Promise")
+})
+.finally(()=>(console.log("Always Excecute")
+))
 
+
+
+new Promise( (resolve, reject)=>{
+    const error = true
+    if (!error){
+        resolve({username: "Hassan", career: "Javascript"})
+    }else{
+        reject("Handle Error using Catch")
+    }
+})
+.then((user)=>{
+    return user.username
+})
+.then( (userName)=>{
+    console.log(userName)
+})
+.catch( (error)=>{
+    console.log(error)
+})
+.finally( ()=>{
+    console.log("Excecute after Reject | Resolve")
+})
+
+
+
+// Same work using Aysnc-Await
