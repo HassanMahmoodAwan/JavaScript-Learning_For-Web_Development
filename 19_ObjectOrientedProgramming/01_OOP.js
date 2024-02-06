cb088857c1b6d3b127720083319a3c06be6da32e
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-06 14:57:04 +0500

    JavaScript OOP Concepts

---
+++ 19_ObjectOrientedProgramming/01_OOP.js
@@ -0,0 +1,42 @@
+// Object Literals   => Object have properties and Methods
+// ==> this(current-Context), this is like --> self.
+
+const userOne = {
+    // Properties
+    username: "Hassan",
+    age: 23,
+    
+    // Methods
+    display: function(){
+        console.log(`${this.username} and ${this.age}`)
+    }
+}
+
+console.log(this)
+console.log(userOne.username)
+console.log(userOne.display());
+
+
+// Note: === what if to create muiliple users. === use constructor Func.
+// to save value in currentContext we use this. in function.
+
+
+function users(username, password, isLoggedIn){
+    this.username = username
+    this.password = password
+    this.isLoggedIn = isLoggedIn
+
+    // Constructor can also hold method.
+    this.logMessage = ()=>(`${this.username} you are logged in`)
+
+    return this     //-> Optional
+}
+
+const UserOne = users("Hassan", "123", true)
+const UserTwo = users("Hamid", "321", true) //->overwrite constru context.
+console.log(UserOne);
+
+// Use new as create separate Instance for each Object.
+const userThree = new users("Ali", "456", false)
+const userFour = new users("Umer", "765", false)
+console.log(userThree);
---
+++ 19_ObjectOrientedProgramming/Notes.md
@@ -0,0 +1,23 @@
+# OOP in JS
+
+- JS is prototype based Language.
+- OOP concepts are behind scene sugar Syntax
+
+- - Objects are collection of Properties and Methods
+
+## OOP Concepts
+
+- Classes and Objects
+- Instances (new, this)
+- Constructor function
+- Prototyping
+
+## 4 pillars of OOP
+
+- Abstraction
+- Encapsulation
+- Inheritance
+- Polymorphism
+
+- - This refers to current Context of Object.
+- - new create new instance of constructor function.
