da67489a35f99c4544c0d941e48a81d7386ecbbd
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-13 12:04:49 +0500

    toLocalDateString

--- 07_Dates_Time/main.js
+++ 07_Dates_Time/main.js
@@ -4,6 +4,7 @@ console.log(newDate, typeof newDate)
 
 console.log(newDate.toString())
 console.log(newDate.toLocaleString())
+console.log(newDate.toLocaleDateString())
 console.log(new Date(2024, 0, 26, 7, 2).toLocaleString())
 console.log(new Date('01-26-2024').getTime())
 console.log(newDate.toDateString())
