723eccfe70fd19b5f5f385b4f20b74c164258ce4
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-06-03 10:32:17 +0500

    Changes while review

--- 02_DataTypes/02_conversion.js
+++ 02_DataTypes/02_conversion.js
@@ -1,11 +1,11 @@
 // ==> All dataTypes to Number
-let score = "23 hello"
+let score = "23 hello 23"
 let firstName = "John"
 let flag = true
 let one;
 let salary = null  //-> 0
 
-console.log(Number(score), typeof Number(score), Number(firstName), 
+console.log(Number(score), parseInt(score),  typeof Number(score), typeof parseInt(score), Number(firstName), 
 typeof Number(firstName)) 
 console.log(Number(flag), typeof Number(flag), Number(salary), typeof Number(salary), 
 Number(one), typeof Number(one)  )
@@ -15,7 +15,7 @@ Number(one), typeof Number(one)  )
 console.log(Boolean(score), typeof Boolean(score), Boolean(firstName), 
 typeof Boolean(firstName)) 
 console.log(Boolean(flag), typeof Boolean(flag), Boolean(salary), typeof Boolean(salary), 
-Boolean(one), typeof Boolean(one)  )
+Boolean(one), typeof Boolean(one), Boolean(-1)  )
 
 /* 
     1- 33 -> true 
